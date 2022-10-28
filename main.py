f18fc5aa9606cf1e8b9dcaa5fc4147a8ad0f6075
IllyaPystovga <illya141312@gmail.com>
2022-10-29 02:50:49 +0300

    added a function for the bot to respond to messages

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ bot = telebot.TeleBot('5673964448:AAGY5_PZS31TYCELZsa1Ve3dC7gL_1GRmS8')
 
 @bot.message_handler(commands=['start'])
 def start(massage):
-    mess = f'Привіт, <b>{massage.from_user.first_name} <u>{massage.from_user.last_name}</u><b>'
-    bot.send_message(massage.chat.id, mess, parse_mode='html')
+    mess = f'Привіт, {massage.from_user.first_name} {massage.from_user.last_name}'
+    bot.send_message(massage.chat.id, mess)
+ # parse_mode='html'
+
+
+@bot.message_handler()
+def get_user_text(message):
+    if message.text == 'Привіт':
+        bot.send_message(message.chat.id, 'I тобі привіт :)')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, f'твій id: {message.from_user.id}')
+    elif message.text == 'скинь фото':
+        photo = open('kotik.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, 'йой щось не зрозуміле, спробуй ще раз:)')
 
 
 bot.polling(none_stop=True)
